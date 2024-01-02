3283b92c31d1c83949f50571ba0b0c499206ad09
drmehmetismail <125666501+drmehmetismail@users.noreply.github.com>
2024-01-02 00:57:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 import os
 import subprocess
 
-def run_script(script_path):
+def prompt_for_path(message):
+    return input(f"Enter the {message}: ").strip()
+
+def run_script(script_path, args):
     try:
-        subprocess.run(['python', script_path], check=True)
+        subprocess.run(['python', script_path] + args, check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while running {script_path}: {e}")
 
 def main():
     base_path = os.path.dirname(os.path.abspath(__file__))
-    # Do not forget to specify input and output directories in each of these scripts:
-    scripts = [
-        'lichess_evals_extractor.py', 
-        'pgn_evaluation_analyzer.py', 
-        'json_to_csv_converter.py', 
-        'chess_data_analyzer.py'
-    ]
-    script_paths = [os.path.join(base_path, script) for script in scripts]
+    # input directory for Lichess DB files (.zst files)
+    lichess_db_input_dir = ''
+    # output directory for PGN files
+    pgn_output_dir = ''
+    # folder path for the output JSON file (from PGN files)
+    bullet_directory = ''
+    json_output_dir = '' 
+    # path for the input JSON file (from PGN files)
+    json_file_path = '.../aggregated_game_data.json'
+    # path for the output CSV file (from JSON file)
+    csv_output_dir = ''
+    # input CSV file path for calculating statistics
+    csv_file_path = '.../aggregated_game_data.csv'
+    # output directory for statistics
+    stats_output_dir = ''
+    # input CSV file for player statistics
+    csv_all_games_path = csv_file_path 
+    # output CSV file for player statistics
+    player_stats_output_dir = ''
 
-    for script_path in script_paths:
-        run_script(script_path)
+    # Define the paths and arguments for each script
+    scripts = [
+        ('lichess_evals_extractor.py', [lichess_db_input_dir, pgn_output_dir, bullet_directory]), 
+        ('pgn_evaluation_analyzer.py', [pgn_output_dir, json_output_dir]),
+        ('json_to_csv_converter.py', [json_file_path, csv_output_dir]),
+        ('chess_stats_summarizer.py', [csv_file_path, stats_output_dir]),
+        ('csv_to_player_stats.py', [csv_all_games_path, player_stats_output_dir])
+    ]
+
+    # Sequentially run the scripts with their full paths and arguments
+    for script_name, args in scripts:
+        script_path = os.path.join(base_path, script_name)
+        print(f"Running {script_path}...")
+        run_script(script_path, args)
 
 if __name__ == "__main__":
     main()
